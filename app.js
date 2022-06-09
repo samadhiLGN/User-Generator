910f2b70651ddd2338998beb8d5de871497f3a3a
Samadhi LGN <samadhimadushanka@gmail.com>
2022-06-09 21:43:57 +0530

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gamil.com',
+            gender: 'male',
+            picture: 'https://factsandfigment.com/wp-content/uploads/2021/07/HQTRGJDSLJDH5IGOQNBATZMFBY.jpg',
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch ('https://randomuser.me/api')
+            const { results } = await res.json ()
+
+            // console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        },
+    },
+})
+
+app.mount('#app')
